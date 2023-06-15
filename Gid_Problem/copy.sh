0a692e93aa4db065478dc97c11b917ad67499f5d
ZLC <licheng.zhang@peraglobal.com>
2023-06-15 17:03:04 +0800

    add multi-material test

--- Gid_Problem/copy.sh
+++ Gid_Problem/copy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-DestPath=$GidPath/problemtypes/TinyFem.gid
 source gid.path
+DestPath=$GidPath/problemtypes/TinyFem.gid
 if [ $# -gt 0 ]
 then
     SrcPath=./TinyFem_$1.gid
     cp $SrcPath/* $DestPath
 fi
+
+#echo $DestPath
+#echo $SrcPath
